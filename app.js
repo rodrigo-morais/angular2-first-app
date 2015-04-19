fe623cafbae02b4abeba7e887abfbef30f8b43d0
Rodrigo Morais <morais.rm@gmail.com>
2015-04-19 01:00:09 -0300

    Following the oficial example in angular2 website.

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿import {Component, Template, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app'
+})
+
+@Template({
+    inline: '<h1>Hello {{ name }}</h1>'
+})
+
+class MyAppComponent {
+    constructor(){
+        this.name = 'Rodrigo';
+    }
+}
+
+bootstrap(MyAppComponent);
